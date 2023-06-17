ee2da457d65b0f6e786f54c1d6c25e9276cf1655
Edder Núñez <phantomon@gmail.com>
2023-06-17 08:08:46 -0700

    improve parsing query strings from request headers

--- keys.js
+++ keys.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 module.exports = {
   database: {
     host: process.env.SQL_HOST,
-    port: process.env.SQL_PORT,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE
--- src/lib/twitchCredentials.js
+++ src/lib/twitchCredentials.js
@@ -1,13 +1,28 @@
 function getTwitchUser ({ 'nightbot-user': user }) {
-  const regex = /.(?<=&|^.).*?(?==)./;
-  const [, name, displayName, provider, providerId, userLevel] = user.split(regex);
-  return { name, displayName, provider, providerId, userLevel };
+  return parse(user)
 }
 
 function getTwitchChannel ({ 'nightbot-channel': channel }) {
-  const regex = /.(?<=&|^.).*?(?==)./;
-  const [, name, displayName, provider, providerId] = channel.split(regex);
-  return { name, displayName, provider, providerId };
+  return parse(channel)
+}
+
+function parse(queryString) {
+  const params = new URLSearchParams(queryString);
+  const object = {};
+
+  for (const [key, value] of params) {
+    if (object[key]) {
+      if (Array.isArray(object[key])) {
+        object[key].push(value);
+      } else {
+        object[key] = [object[key], value];
+      }
+    } else {
+      object[key] = value;
+    }
+  }
+
+  return object;
 }
 
 module.exports = { getTwitchUser, getTwitchChannel };
